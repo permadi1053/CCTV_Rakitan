29e12318f8d6e535fe6a1dd7aae0270fac451d50
Sigit Permadi <116578793+permadi1053@users.noreply.github.com>
2025-01-03 20:12:23 +0700

    Create bot_telegram.sh

---
+++ motioneye/bot_telegram.sh
@@ -0,0 +1,8 @@
+BOT_TOKEN="Bot toke"
+SNAPSHOT_URL="URL Snapshoot"
+USER_CHAT_ID="User ID Telegram"
+CAPTION="Motion Detected"
+
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+curl -o "${SCRIPT_PATH}/output.png" $SNAPSHOT_URL
+curl -X POST -F "photo=@${SCRIPT_PATH}/output.png;type=image/png" "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto?chat_id=${USER_CHAT_ID}&caption=${CAPTION}"
